c85914af6d5f2d6ec053089d9b5417e5df4ebba7
ducannh <anhductranminh@gmail.com>
2025-02-15 22:25:51 +0700

    energy enemy

---
+++ Assets/Game/Scrips/energyEnemy.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class energyEnemy : Enemy
+{
+    [SerializeField] private GameObject energyObject;
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.CompareTag("Player")){
+            player.TakeDamage(20);
+        }
+    }
+    private void OnTriggerStay2D(Collider2D other) {
+        if(other.CompareTag("Player")){
+            player.TakeDamage(20);
+        }
+    }
+
+    protected override void Die()
+    {
+        if(energyObject != null){
+            GameObject energy = Instantiate(energyObject,transform.position,Quaternion.identity);
+            Destroy(energy, 5f);
+        }
+        base.Die();
+    }
+}
